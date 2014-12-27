240fee1eafba780bcf93e2102f7a2b122e8e354a
presian <presiandanailov@gmail.com>
2014-12-27 12:34:36 +0200

    Remove Seed

--- AngularJS-AdsProject/app/modules/cs_messenger/main.js
+++
@@ -1,9 +0,0 @@
-define([
-  './module',
-
-  // Directives
-  './scripts/messenger_directive',
-
-  // Providers
-  './scripts/messenger_provider'
-], function() {});
--- AngularJS-AdsProject/scripts/bs-tunnel-connect.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -jar scripts/BrowserStackTunnel.jar YOUR_KEY localhost,9090,0
